529098a8ca9c7b18b1cf962c138fd0ab8d41e148
Paul Johnson <paul.johnson@touchstar.co.uk>
2025-06-12 10:36:32 +0100

    updated for path to the  app instance

--- TheWeatherApp/Platforms/Android/MainActivity.cs
+++ TheWeatherApp/Platforms/Android/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 
@@ -7,5 +8,17 @@ namespace TheWeatherApp
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        public static Activity? Active { get; private set; }
+
+        public static ISharedPreferences? Prefs { get; set; }
+
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            Active = this;
+            Prefs = Active.GetSharedPreferences("weather", FileCreationMode.Private);
+
+            base.OnCreate(savedInstanceState);
+            RequestedOrientation = ScreenOrientation.Portrait;
+        }
     }
 }
--- TheWeatherApp/Platforms/iOS/AppDelegate.cs
+++ TheWeatherApp/Platforms/iOS/AppDelegate.cs
@@ -1,10 +1,24 @@
 ﻿using Foundation;
+using UIKit;
 
 namespace TheWeatherApp
 {
     [Register("AppDelegate")]
     public class AppDelegate : MauiUIApplicationDelegate
     {
+        public static AppDelegate Self { get; private set; }
+        public NSUserDefaults UserDefaults { get; private set; }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            AppDelegate.Self = this;
+            UserDefaults = new NSUserDefaults("group.uk.co.all-the-johnsons.theweatherapp");
+
+            return base.FinishedLaunching(app, options);
+        }
+
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 }
