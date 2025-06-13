15d9d5554dfe4f4240b9d1e68482a8191c37efb6
Paul Johnson <paul.johnson@touchstar.co.uk>
2025-06-13 16:03:12 +0100

    Add initcode

--- TheWeatherApp/App.xaml.cs
+++ TheWeatherApp/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace TheWeatherApp
+﻿using TheWeatherApp.ViewModels;
+
+namespace TheWeatherApp
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            Startup.ServiceProvider.GetService<BaseViewModel>().FirstRun();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
