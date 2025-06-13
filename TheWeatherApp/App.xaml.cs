b7e87ff60634da24d112c0626b8ec5e6ff3cc485
Paul Johnson <paul.johnson@the49.com>
2025-06-13 23:12:10 +0100

    add code for compilation

--- TheWeatherApp/App.xaml.cs
+++ TheWeatherApp/App.xaml.cs
@@ -1,18 +1,80 @@
-﻿using TheWeatherApp.ViewModels;
+﻿using System.Globalization;
+using SQLite;
+using TheWeatherApp.Database;
+using TheWeatherApp.Interfaces;
+using TheWeatherApp.ViewModels;
 
 namespace TheWeatherApp
 {
     public partial class App : Application
     {
+        public static IServiceProvider Service { get; set; }
+        public bool IsConnected { get; private set; }
+        public static App Self { get; private set; }
+        public static Size ScreenSize { get; private set; }
+        public static SQLiteAsyncConnection SQLConnection { get; set; }
+        
+        bool isStarted { get; set; } = false;
         public App()
         {
+            App.Self = this;
+
+            Service = Startup.Init();
+
+            ScreenSize = new Size(DeviceDisplay.MainDisplayInfo.Width, DeviceDisplay.MainDisplayInfo.Height);
             InitializeComponent();
             Startup.ServiceProvider.GetService<BaseViewModel>().FirstRun();
+            
+            var connect = (Connectivity.NetworkAccess == NetworkAccess.Internet) || (Connectivity.NetworkAccess == NetworkAccess.Local);
+
+            Service.GetService<BaseViewModel>().IsConnected = IsConnected = Connectivity.NetworkAccess == NetworkAccess.Internet;
+
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+            
+            SetupSQLiteConnection();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(new Views.TabbedView());
+        }
+        
+        void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            var connect = Connectivity.NetworkAccess == NetworkAccess.Internet;
+
+            Service.GetService<BaseViewModel>().IsConnected = IsConnected = connect;
+        }
+        
+        protected override void OnStart()
+        {
+            if (!isStarted)
+            {
+                isStarted = true;
+                var netLanguage = Service.GetService<ILocalize>().GetCurrent();
+                Languages.Resources.Culture = new CultureInfo(netLanguage);
+                Service.GetService<ILocalize>().SetLocale();
+
+                new SqLiteRepository();
+            }
+        }
+
+        void SetupSQLiteConnection()
+        {
+            var path = Path.Combine(FileSystem.AppDataDirectory, "weather.db3");
+
+            var options = new SQLiteConnectionString(path, SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite, true, 
+                "42b5d84353894794804881b5b6f398c9");
+            try
+            {
+                App.SQLConnection = new SQLiteAsyncConnection(options);
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Console.WriteLine($"Connection could  not be made : ex.Message = {ex.Message} - inner = {ex.InnerException?.Message}");
+#endif
+            }
         }
     }
 }
