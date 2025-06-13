855066eec8412223bbd78936d14a3050e1d7aedf
Paul Johnson <paul.johnson@touchstar.co.uk>
2025-06-13 16:00:08 +0100

    Add constants

---
+++ TheWeatherApp/Constants/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TheWeatherApp.Constants
+{
+    public class Constants
+    {
+        public static string APIKey { get; private set; } = "7a6e5207245240959ea50859251206";
+        public static string BaseUrl { get; private set; } = "http://api.weatherapi.com/v1";
+    }
+}
